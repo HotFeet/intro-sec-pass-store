b33f53093ddc7e349654ff49b789997f88956c61
jskripsky <juraj.skripsky@gmail.com>
2011-02-15 10:48:11 +0100

    Add comments and a print action.

--- obscurity-sample.cs
+++ obscurity-sample.cs
@@ -6,15 +6,19 @@ using c = System.Char;
 
 static class Crypto {
 	static void Main(s[] args) {
+		// helper functions
 		Func<s, c[]> explode = s => s.ToCharArray();
 		Func<c[], s> implode = chrs => new string(chrs);
+		Action<s> print = s => Console.WriteLine(s);
 
+		// encryption
 		Func<c, c> shift = chr => (c)(chr + 1);
 		Func<c[], c[]> scramble =
 			chrs => chrs.Reverse().Select(chr => shift(chr)).ToArray();
 
+		// read input, encrypt, write output
   		c[] input = explode(args[0]);
   		c[] output = scramble(input);
-  		Console.WriteLine(implode(output));
+  		print(implode(output));
 	}
 }
