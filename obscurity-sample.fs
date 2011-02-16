2dea34ee013b096b4c2c661595bbac0b690c2fee
jskripsky <juraj.skripsky@gmail.com>
2011-02-16 12:15:56 +0100

    Refine F# sample.

--- obscurity-sample.fs
+++ obscurity-sample.fs
@@ -1,5 +1,13 @@
+open System
+
 let explode (s: string) = s |> Seq.toArray
 let implode (cs: char[]) = new string(cs)
 
-let shiftChar c = (char)((int)c + 1)
-let scramble = explode >> Array.rev >> Array.map shiftChar >> implode
+let switchCase c = 
+	match c with
+	| _ when Char.IsUpper c -> Char.ToLower c
+	| _ when Char.IsLower c -> Char.ToUpper c
+	| _ -> c
+
+let shiftChar dist (c: char) = (char)((int)c + dist)
+let scramble = explode >> Array.rev >> Array.map switchCase >> Array.map (shiftChar 1) >> implode
